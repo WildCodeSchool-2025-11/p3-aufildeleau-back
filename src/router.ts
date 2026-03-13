ba4f776139a8a1e3937a0dd4cc294761e1ec2d85
yavuzkutuk <yavuz@kutuk.fr>
2026-03-13 11:17:50 +0100

    feat: insertion route auth

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import authRoutes from "./modules/auth/authRoutes.js";
 import itemsRoutes from "./modules/items/itemsRoutes.js";
 
 const router = Router();
 
 router.use("/api/items", itemsRoutes);
+router.use("/api/auth", authRoutes);
 
 export default router;
