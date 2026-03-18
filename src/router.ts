8fd646cd17b0c2fa5e76093393b0127f225939b1
yavuzkutuk <yavuz@kutuk.fr>
2026-03-18 14:22:44 +0100

    feat: insertion login crypté

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import authRoutes from "./modules/auth/authRoutes.js";
 import itemsRoutes from "./modules/items/itemsRoutes.js";
+import { checkLogin } from "./middlewares/authMiddleware.js";
 
 const router = Router();
 
 router.use("/api/items", itemsRoutes);
-router.use("/api/auth", authRoutes);
+router.use("/api/auth", checkLogin,  authRoutes);
 
 export default router;
